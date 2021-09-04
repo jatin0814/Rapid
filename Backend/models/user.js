004d019562a053fbe702a2711e923efbd4176b24
unknown <201951073@iiitvadodara.ac.in>
2021-09-04 19:22:45 +0530

    user controller added

---
+++ Backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema
+
+const userSchema = new schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true
+    },phoneNo:{
+        type:String,
+        required:true
+    },isPolice:{
+        type:Boolean,
+        default:false
+    },FIRs:{
+        type:Array,
+        default:[]
+    }
+})
+
+
+module.exports = mongoose.model("User",userSchema)
