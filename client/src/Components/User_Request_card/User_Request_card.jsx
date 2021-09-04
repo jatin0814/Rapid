7f8013f17f5e5bd6f36cfec9279ecd405660d038
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2021-09-05 01:39:49 +0530

    user component changes

--- client/src/Components/User_Request_card/User_Request_card.jsx
+++ client/src/Components/User_Request_card/User_Request_card.jsx
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 import { PinDropSharp } from "@material-ui/icons";
 
 function User_Request_Card(props) {
-
   return (
-    <div>
-
-      <div>{props.id}</div>
+    <div
+      style={{
+        width: "100%",
+        height: "100px",
+        background: props.darkTheme ? "#000" : "#e0dfda",
+        marginBottom: "25px",
+        borderRadius: "10px",
+        padding: "15px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        cursor: "pointer",
+        boxShadow: `0 3px 10px rgb(0 0 0 / 0.2)`,
+      }}
+    >
+      <div>
+        <div style={{ fontWeight: "bold" }}>Request ID: {props.id}</div>
+        <div>Description: {props.facts}</div>
+      </div>
       <div>{props.date}</div>
-      <div>{props.facts}</div>
-
     </div>
-    
-  ) 
+  );
 }
 
-
 export default User_Request_Card;
-
-
-
